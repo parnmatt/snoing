bd1c5dd6b863c4ad1090d4f23375de26b8b779af
Matthew Parnell <matt@parnmatt.co.uk>
2016-06-23 18:06:45 -0400

    Allow for forks of snoing
    Previously hardcoded to reset origin remote to snoplus/snoing.
    
    If origin is snoplus/snoing then pull, otherwise, enforce upstream
    remote is snoplus/snoing (master only), and fetch upstream.
    This follows from the rat-dev convention:
        origin:   username/rat
        upstream: snoplus/rat
    
    This ensures that the user will always have the up-to-date 'offical'
    version, reguardless of their fork.
    
    Check if internet connection exists before needing to use it.
    
    Added `cleanup` to unset declared functions and variables from
    global scope.

--- env.sh
+++ env.sh
@@ -1,11 +1,29 @@
 #!/bin/echo This script should be sourced:
 
-printf "snoing setup\n"
+printf "Setup snoing\n"
+
+function cleanup {
+    unset internet snoing_upstream_url
+    unset cleanup internet_connection
+}
+
+function internet_connection {
+    number_of_tries=${1:-3}
+
+    if [[ -z $internet ]]; then
+        printf "%-50s" "Checking for internet, $number_of_tries passes..."
+        internet=$(ping -c$number_of_tries 8.8.8.8 >/dev/null 2>&1; printf $?)
+        [[ $internet == 0 ]] && printf "found\n" || printf "not found\n"
+    fi
+    return $internet
+}
+
 export PYTHONPATH=$PWD:$PWD/core:$PWD/packages:$PWD/versions:$PYTHONPATH
 
 printf "%-50s" "Checking for git..."
 if ! command -v git >/dev/null 2>&1; then
     printf "Not Installed\n"
+    cleanup
     return
 fi
 printf "Installed\n"
@@ -13,8 +31,36 @@ printf "Installed\n"
 printf "%-50s" "Checking if this is a git repository..."
 if [[ $(git rev-parse --is-inside-work-tree 2>/dev/null) != true ]]; then
     printf "no\n"
+    cleanup
     return
 fi
-printf "yes\nAttempting update via git pull...\n"
-git remote set-url origin git@github.com:snoplus/snoing.git
+printf "yes\n"
+
+snoing_upstream_url="git@github.com:snoplus/snoing.git"
+if [[ $(git config remote.origin.url) != $snoing_upstream_url ]]; then
+    printf "Using custom snoing\n"
+    printf "Checking upstream\n"
+    if ! git config remote.upstream.url >/dev/null; then
+        git remote add -t master upstream $snoing_upstream_url
+    else
+        git remote set-url upstream $snoing_upstream_url
+    fi
+
+    if ! internet_connection; then
+        cleanup
+        return
+    fi
+    git fetch upstream
+else
+    printf "Using upstream snoing\n"
+fi
+
+if ! internet_connection; then
+    cleanup
+    return
+fi
+printf "Attempting update via git pull...\n"
 git pull
+
+cleanup
+return
